4c6e5523c0adae43b285f04c7ddc08b5898979ba
Rui Fernandes <ruipedro16@protonmail.com>
2022-01-03 15:52:11 +0000

    TPAS - update secretservice.sh

--- TPAS/Challenges/secretservice.sh
+++ TPAS/Challenges/secretservice.sh
@@ -3,6 +3,6 @@
 nmap -sV -p 5000-7000 tpas.alunos.dcc.fc.up.pt -oN tmp.txt >/dev/null 2>&1
 
 # 5015, the first port running http, hosts the website for the "Bug Bounty" challenge
-PORT=$(grep -E "open[[:space:]]+http" tmp.txt | tail -n 1 | cut -d '/' -f 1)
+PORT=$(grep -E "open[[:space:]]+http" tmp.txt | tail -n 1 | cut -d '/' -f 1) # PORT = 5037
 curl --insecure --silent "tpas.alunos.dcc.fc.up.pt:$PORT" | grep -oE "TPAS{.+}" 
 rm tmp.txt
